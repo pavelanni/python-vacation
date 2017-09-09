b7140d2ce37e0067272b4d5a5c15a5940cea0abe
Pavel Anni <pavel.anni@gmail.com>
2017-09-09 15:04:21 -0500

    Deployed 22b60c7 with MkDocs version: 0.16.3

--- coffeeshop-day5.py
+++ coffeeshop-day5.py
@@ -14,24 +14,26 @@ toppings = ["chocolate", "sweetstrawberry", "caramel"]
 
 def menu(options):
     good_input = False
-    while good_input is False:
+    while good_input == False:
         for i, option in enumerate(options, start=1):
             print(i, option)
         choice = input()
 
         try:
             choice_int = int(choice)
-        except ValueError:
+        except  ValueError:
             print("Error: you should enter a NUMBER!")
             continue
 
-        if choice_int <= len(options) and choice_int > 0:
+        if choice_int <= len(options) and choice_int > 0 :
             good_input = True
         else:
             print("You should enter a number between 1 and ", len(options))
 
     return choice_int-1
 
+
+name = input("What's your name: ")
 
 print("Choose your base product from the menu (press the number):")
 p_index = menu(products)
@@ -42,5 +44,9 @@ f_index = menu(flavors)
 print("Choose your topping from the menu (press the number):")
 t_index = menu(toppings)
 
-print("Here is your order: base product: ", products[p_index],
-      ", flavor: ", flavors[f_index], ", topping: ", toppings[t_index])
+print("Hello ", name, "! Here is your order: base product: ", products[p_index], 
+  ", flavor: ", flavors[f_index], ", topping: ", toppings[t_index])
+
+f = open("orders.txt", "a+")
+f.write(",".join((name, products[p_index], flavors[f_index], toppings[t_index]))+"\n")
+f.close()
